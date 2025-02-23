d28cd6762b744d152755b07bb112da111afeffe9
julianzp <zapataparrajulian@gmail.com>
2025-02-23 18:41:55 -0500

    Functional server v-1.0

--- momentos_evaluativos/server/src/main/java/com/example/server/ServerApplication.java
+++ momentos_evaluativos/server/src/main/java/com/example/server/ServerApplication.java
@@ -1,13 +1,28 @@
 package com.example.server;
-
+import com.example.implement.EmpresaImpl;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws RemoteException {
+		Registry reg = LocateRegistry.createRegistry(1099);
+
+		// Crear e implementar el objeto remoto
+		EmpresaImpl empresaImplement = new EmpresaImpl();
+		reg.rebind("empresa", empresaImplement);
+
+		System.out.println("Objeto remoto 'empresa' registrado correctamente.");
+
+		System.out.println("\n Servidor RMI iniciado y listo para recibir solicitudes.");
+
 		SpringApplication.run(ServerApplication.class, args);
+
+
 	}
 
 }
